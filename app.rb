9fe9ba78527dc020b0ce19c85aeb8dd59580ffdc
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-13 21:19:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is ___"
+  end 
+
+  get '/hometown' do 
+  end 
+  
+  get '/favorite-song' do
+  end 
+  
 end
